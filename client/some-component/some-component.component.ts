fd8ee6671ffb3ba3c4099a15ce3b1933e0798e14
Derek Caswell <dcaswell@goreact.com>
2023-01-31 19:06:58 -0700

    Added further explanation

--- client/some-component/some-component.component.ts
+++ client/some-component/some-component.component.ts
@@ -18,7 +18,8 @@ export class SomeComponentComponent implements OnInit {
     /**
      * Currently in my PhpStorm, I get an error on line 16 that says there is no overload for this call. If I comment out
      * line 2 and uncomment line 3, that error goes away but then a new error is shown on line 3 saying that there's no
-     * exported member called map.
+     * exported member called map. If I rename the node_modules folder on the root level, then everything works fine so that
+     * tells me that it's using the wrong one.
      */
   }
 }
